cf8906c0d37c13cc7f90066f8b134f8f633dba33
zhikangm <43835526+zhikangm@users.noreply.github.com>
2023-10-31 13:52:07 -0500

    first commit

---
+++ Sources/GroceryAppSharedDTO/LoginResponseDTO.swift
@@ -0,0 +1,23 @@
+//
+//  LoginResponseDTO.swift
+//  
+//
+//  Created by zhikang meng on 10/31/23.
+//
+
+import Foundation
+
+public struct LoginResponseDTO: Codable {
+    
+    public let error: Bool
+    public var reason: String? = nil
+    public var token: String? = nil
+    public var userId: UUID? = nil
+    
+    public init(error: Bool, reason: String? = nil, token: String? = nil, userId: UUID? = nil) {
+        self.error = error
+        self.reason = reason
+        self.token = token
+        self.userId = userId
+    }
+}
---
+++ Sources/GroceryAppSharedDTO/RegisterResponseDTO.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterResponseDTO.swift
+//  
+//
+//  Created by zhikang meng on 10/31/23.
+//
+
+import Foundation
+
+public struct RegisterResponseDTO: Codable {
+    
+    public let error: Bool
+    public var reason: String? = nil
+    
+    public init(error: Bool, reason: String? = nil) {
+        self.error = error
+        self.reason = reason
+    }
+    
+}
