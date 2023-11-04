6c15636e2eecaa51a357c0c6db2570d5a52c9b76
zhikangm <43835526+zhikangm@users.noreply.github.com>
2023-11-04 12:37:52 -0500

    add public

--- Sources/GroceryAppSharedDTO/GroceryCategoryResponseDTO.swift
+++ Sources/GroceryAppSharedDTO/GroceryCategoryResponseDTO.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct GroceryCategoryResponseDTO: Codable {
+public struct GroceryCategoryResponseDTO: Codable {
     public let id: UUID
     public let title: String
     public let colorCode: String
