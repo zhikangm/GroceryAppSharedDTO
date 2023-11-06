df6ef74716e49b8344cb18feaea57b24e9d101bb
zhikangm <43835526+zhikangm@users.noreply.github.com>
2023-11-05 22:01:02 -0600

    update optional

--- Sources/GroceryAppSharedDTO/GroceryCategoryResponseDTO.swift
+++ Sources/GroceryAppSharedDTO/GroceryCategoryResponseDTO.swift
@@ -11,12 +11,20 @@ public struct GroceryCategoryResponseDTO: Codable {
     public let id: UUID
     public let title: String
     public let colorCode: String
-    public let items: [GroceryItemResponseDTO]
+    // OPTIONAL: Get all grocery categories with their items
+    //public let items: [GroceryItemResponseDTO]
     
-    public init(id:UUID, title: String, colorCode: String, items: [GroceryItemResponseDTO] = [] ){
+    public init(id:UUID, title: String, colorCode: String){
         self.id = id
         self.title = title
         self.colorCode = colorCode
-        self.items = items
     }
+    
+//    public init(id:UUID, title: String, colorCode: String, items: [GroceryItemResponseDTO] = [] ){
+//        self.id = id
+//        self.title = title
+//        self.colorCode = colorCode
+//        self.items = items
+//    }
+    
 }
